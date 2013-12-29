c4c84425cc4682904d166b3b6bcfb04307e5585f
necromorph <thomas.malkewitz@gmail.com>
2013-12-29 12:13:47 -0500

    Added Get-TpmStatus Function
    Changed Get-BitLockerStatus Function to return BOOL Value, and the made
    the Write-Host actions only dislplay if the -Verbose Parameter is
    passed.  The Get-TpmStatus Function works the same.

--- BitLocker.psm1
+++ BitLocker.psm1
@@ -1,8 +1,81 @@
-﻿function Get-BitLockerStatus {
+﻿function Get-TpmStatus {
     <#
     .SYNOPSIS
 
-    This function is to be used to easy determine the BitLocker status of a drive.
+    This function is to be used to easily determine the TPM (Trusted Platform Module) Status on a machine.
+
+    Function: Get-TpmStatus
+    Author: Thomas Malkewitz MCP, MCDST, CompTIA A+
+    Required Dependencies: None
+    Optional Dependencies: None
+    Version: 0.3 
+
+    .DESCRIPTION
+
+    This function returns true if the TPM is On and Enabled, and false for all other seneros, the -Verbose parameter will provide user friendly STDOUT information.
+
+    .PARAMETER ComputerName
+
+    String.  This is the Computer Name where the Volume is located, default is the local computer running the function.
+
+    .EXAMPLE
+    
+    Get-TpmStatus
+
+    In this example, the TPM Status is returned of the system drive on the local machine.
+
+    .EXAMPLE 
+
+    Get-TpmStatus -ComputerName "MyComputer.MyDomain.org" -Verbose
+
+    In this example, the TPM Status is returned from a remote computer with user friendly readable information.
+
+    #>
+    
+    [CmdletBinding()]  
+    param 
+        (
+        [String] $ComputerName = $env:COMPUTERNAME
+        )
+
+    if (!(Test-Connection -ComputerName $ComputerName -Quiet -Count 2)) {
+        Throw 'Unable to connect to ' + $ComputerName + '.  Please ensure the system is available, and that you have sufficent rights to connect to the Remote Windows Management Interface.'
+        return $false
+        }
+
+    try {
+        $tpm = Get-WmiObject -Class Win32_Tpm -Namespace "root\CIMV2\Security\MicrosoftTpm" -ComputerName $ComputerName -ErrorAction Stop
+        }
+    catch {
+        Throw 'Unable to connect to the "Win32_Tpm" Namespace, You may not have sufficent rights.'
+        return $false
+        }
+
+    if (!($tpm.IsEnabled_InitialValue)) {
+        if ($VerbosePreference -eq "Continue") {
+            Write-Host "TPM is not Enabled."
+            }
+        return $false
+        }
+    elseif (!($tpm.IsActivated_InitialValue)) {
+        if ($VerbosePreference -eq "Continue") {
+            Write-Host "TPM is Enabled, but not Activated."
+            }
+        return $false
+        }
+    else {
+        if ($VerbosePreference -eq "Continue") {
+            Write-Host "TPM is both Enabled and Activated."
+            }
+        return $true
+        }
+    }
+
+function Get-BitLockerStatus {
+    <#
+    .SYNOPSIS
+
+    This function is to be used to easily determine the BitLocker status of a drive.
 
     Function: Get-BitLockerStatus
     Author: Thomas Malkewitz MCP, MCDST, CompTIA A+
@@ -12,7 +85,7 @@
 
     .DESCRIPTION
 
-    This function is for User Friendly STDOUT of the BitLocker Drive Encryption (BDE) of a drive.  No values are returned, only Write-Host values of the BDE Status.
+    This function returns true if the Protection Status is ON, and false for all other seneros, the -Verbose parameter will provide friendly STDOUT information.
 
     .PARAMETER ComputerName
 
@@ -23,12 +96,16 @@
     String.  This is the Drive Letter to evalute BDE on, default is the System Drive.  (Which is evalutated through, WMI, the $env:SYSTEMDRIVE varible is not used.)
 
     .EXAMPLE
+    
+    Get-BitLockerStatus
 
-    Get-BitLockerStatus -ComputerName $env:COMPUTERNAME
+    In this example, the BDE Status is returned of the system drive on the local machine.
 
-    .EXAMPLE
+    .EXAMPLE 
 
     Get-BitLockerStatus -ComputerName "MyComputer.MyDomain.org" -DriveLetter "C:"
+
+    In this example, the BDE Status is returned from the C: drive on remote computer.
 
     #>
     
@@ -41,6 +118,7 @@
 
     if (!(Test-Connection -ComputerName $ComputerName -Quiet -Count 2)) {
         Throw 'Unable to connect to ' + $ComputerName + '.  Please ensure the system is available, and that you have sufficent rights to connect to the Remote Windows Management Interface.'
+        return $false
         }
 
     if ([String]::IsNullOrEmpty($DriveLetter)) {
@@ -50,6 +128,7 @@
             }
         catch {
             Throw 'Unable to connect to the necassary WMI Namespaces, to get the system drive.  Verfy that you have sufficent rights to connect to the "OperationSystem" and "EncryptableVolume" Namespaces.'
+            return $false
             }
         }
     else {
@@ -59,17 +138,34 @@
         $volume = Get-WmiObject -Class Win32_EncryptableVolume -Namespace "root\CIMV2\Security\MicrosoftVolumeEncryption" -Filter "DriveLetter = '$DriveLetter'" -ComputerName $ComputerName -ErrorAction Stop
         if ($volume -eq $null) {
             Throw 'Unable to enumarate the "EncryptableVolume" WMI Namespace for drive ' + $DriveLetter + '.  Please make sure the drive letter is correct and that the volume is accessable.'
+            return $false
             }
         }
 
     $status = $volume.GetConversionStatus()
-    switch ($status.ConversionStatus) {
-        0 { Write-Host "FullyDecrypted"; Write-Host "ProtectionOff" }
-        1 { Write-Host "FullyEncrypted"; if ($volume.ProtectionStatus -eq 1) { Write-Host "ProtectionOn" }; else { Write-Host "ProtectionOff" } }
-        2 { Write-Host "EncryptionInProgress" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage; Write-Host "ProtectionOff" }
-        3 { Write-Host "DecryptionInProgress" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage; Write-Host "ProtectionOff" }
-        4 { Write-Host "EncryptionPaused" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage; Write-Host "ProtectionOff" }
-        5 { Write-Host "DecryptionPaused" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage; Write-Host "ProtectionOff" }
+
+    if ($VerbosePreference -eq "Continue") {
+        switch ($status.ConversionStatus) {
+            0 { Write-Host "FullyDecrypted" }
+            1 { Write-Host "FullyEncrypted" }
+            2 { Write-Host "EncryptionInProgress" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage }
+            3 { Write-Host "DecryptionInProgress" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage }
+            4 { Write-Host "EncryptionPaused" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage }
+            5 { Write-Host "DecryptionPaused" -NoNewline; Write-Host "  PercentageComplete: " $status.EncryptionPercentage }
+            }
+        }
+        
+    if ($volume.GetProtectionStatus().ProtectionStatus -eq 0) {
+        if ($VerbosePreference -eq "Continue") {
+            Write-Host "ProtectionOff"
+            }
+        return $false
+        }
+    else {
+        if ($VerbosePreference -eq "Continue") {
+            Write-Host "ProtectionOn"
+            }
+        return $true
         }
     }
 
